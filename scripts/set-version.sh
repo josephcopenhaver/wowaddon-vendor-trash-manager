56a6777ef196475b8c67f5ac1a80f8e9c6cbdceb
Joseph Patrick Copenhaver <joseph.copenhaver@gmail.com>
2019-12-26 12:45:11 -0600

    Support for making a release without touching github

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -4,12 +4,14 @@ set -exo pipefail
 
 # constants
 toc_file="VendorTrashManager.toc"
+relnotes_file="release-notes.md"
 
 version="${1}"
+release_notes="${2}"
 
 # verify project state is consistent before this script runs
 
-if [ -n "$(git diff --name-only | grep -v "${toc_file}")" ] || [ -n "$(git ls-files --others --exclude-standard)" ]; then
+if [ -n "$(git diff --name-only | grep -v "${toc_file}" | grep -v "${relnotes_file}")" ] || [ -n "$(git ls-files --others --exclude-standard | grep -v "${relnotes_file}")" ]; then
     git status
     echo ""
     echo "dirty or untracked files exist"
@@ -22,10 +24,19 @@ if [[ "${version}" = "${prefix}"* ]]; then
     version="${version#"${prefix}"}"
 fi
 
-test -n "${version}" || (
+if [ -z "${version}" ]; then
     echo "no version specified"
     exit 1
-)
+fi
+
+if [ -z "${release_notes}" ] && [ -f "${relnotes_file}" ]; then
+    release_notes="$(cat "${relnotes_file}")"
+    if [ -z "${release_notes}" ]; then
+        echo ""
+        echo "[WARNING]: release notes file '${relnotes_file}' is empty, you should probably delete it"
+        echo ""
+    fi
+fi
 
 version="v${version}"
 
@@ -39,3 +50,13 @@ git tag "${version}"
 
 git push origin
 git push origin "refs/tags/${version}"
+
+release_opts=""
+if [ -n "${release_notes}" ]; then
+    release_opts="-m $(printf '%q' "${release_notes}")"
+else
+    echo "no release notes found, making a pre-release"
+    release_opts="-p"
+fi
+
+hub release create -m "${version}" ${release_opts} "${version}"
